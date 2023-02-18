4b5c909001abea0b7501d10dbb7e502b0f632232
Kevin C <48696458+kcruzs@users.noreply.github.com>
2023-02-17 22:30:17 -0600

    Update Main.java

--- tarea_1/Main.java
+++ tarea_1/Main.java
@@ -1,5 +1,15 @@
-public class Main{
-    public static void main(String[] args) {
-        /* Instanciar arbol */
+
+    public class Main{
+        public static void main(String[] args) {
+    
+            /* Pruebas  para recorrer en InOrden*/
+            Arbol arbol = new Arbol();
+            arbol.insertar(4,"100");
+             arbol.insertar(8,"200");
+             arbol.insertar(1,"300");
+             arbol.insertar(9,"400");
+             arbol.insertar(7,"500");
+         
+            arbol.recorrer(arbol.raiz);
+        }
     }
-}
