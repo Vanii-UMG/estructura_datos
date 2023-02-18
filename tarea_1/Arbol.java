1552dccc3f0c648a0003dfab83e13c38e9a71407
Kevin C <48696458+kcruzs@users.noreply.github.com>
2023-02-17 22:26:29 -0600

    Update Arbol.java

--- tarea_1/Arbol.java
+++ tarea_1/Arbol.java
@@ -39,24 +39,4 @@ public void insertar(int i, Object dato){
         }
     }
     
-    
-    
-    
-    private class Nodo{
-       public Nodo padre;
-        public Nodo derecha;
-        public Nodo izquierda;
-        public int llave;
-        public Object contenido;
-        
-        
-        public Nodo (int indice){
-        llave = indice;
-            derecha = null;
-            izquierda = null;
-            padre = null;
-            contenido = null;
-        }
-    
-    }
 }
